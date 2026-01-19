9cac706bc4a75f5cd28e916e9cef56786b692646
tomoya <kakakaktk@gmail.com>
2026-01-19 20:14:43 +0900

    サウナ施設等検索機能の実装（レイアウト関係)

--- app/javascript/map.js
+++ app/javascript/map.js
@@ -1,9 +1,10 @@
 export function initMap() {
     const target = document.getElementById("map");
+    // エラー防止のため
     if (!target) return;
 
     const map = new google.maps.Map(target, {
-        center: { lat: 35.6812, lng: 139.7671 },
+        center: { lat: 35.412715, lng: 136.771715},
         zoom: 15,
         mapid: "DEMO_MAP_ID"
     });
