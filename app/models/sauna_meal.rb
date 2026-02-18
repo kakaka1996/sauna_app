299b1ca2f7deeb036a13829a9bb9c573dd86c8bc
tomoya <kakakaktk@gmail.com>
2026-02-18 17:00:57 +0900

    fix lintcheckに対する修正

--- app/models/sauna_meal.rb
+++ app/models/sauna_meal.rb
@@ -1,6 +1,6 @@
 class SaunaMeal < ApplicationRecord
-#店名、※本リリース時には飯画像も上げられるようにする
-  validates: :restaurant, presence: true, length: {maximum: 50}
-  
+  # 店名、※本リリース時には飯画像も上げられるようにする
+  validates :restaurant, presence: true, length: { maximum: 50 }
+
   belong_to :sauna_log
 end
--- app/models/sauna_set.rb
+++ app/models/sauna_set.rb
@@ -1,11 +1,11 @@
 class SaunaSet < ApplicationRecord
-#サウナ室時間、サウナ室温度、水風呂時間、水風呂温度、外気浴時間
-validates: :heat_time, presence: true, numericality: {only_integer: true, greater_than_or_equal_to: 1, :less_than_or_equal_to: 25}
-validates: :heat_temperature, presence: true, numericality: { greater_than_or_equal_to: 30, :less_than_or_equal_to: 120}
-validates: :water_bath_time, presence: true, numericality: {only_integer: true, greater_than_or_equal_to: 1, :less_than_or_equal_to: 5}
-validates: :water_bath_temperature, presence: true, numericality: {only_integer: true, greater_than_or_equal_to: 0, :less_than_or_equal_to: 25}
-validates: :rest_time, presence: true numericality: {only_integer: true, greater_than_or_equal_to: 1, :less_than_or_equal_to: 60}
+# サウナ室時間、サウナ室温度、水風呂時間、水風呂温度、外気浴時間
+validates :heat_time, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 25 }
+validates :heat_temperature, presence: true, numericality: { greater_than_or_equal_to: 30, less_than_or_equal_to: 120 }
+validates :water_bath_time, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 5 }
+validates :water_bath_temperature, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 25 }
+validates :rest_time, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 60 }
 
 
-belong_to :sauna_log
+belongs_to :sauna_log
 end
